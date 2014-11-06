6d02eb3cb4186f2e3811cc87f1ed30d507be0b2a
armiller1 <aubriana.miller@yahoo.com>
2014-11-06 18:08:45 -0500

    Expressions_Industry
    This is the correct submission

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,4 +5,20 @@
  11-6-2014
  */
 
+//How many Days until your Anniversary
+var anniVers = 347;
+prompt("When is your Anniversary?");
+
+var currDate = 310;//today's date
+prompt("What is today's date?");
+
+var daysTill = anniVers - currDate + " days until your Anniversary!";
+
+alert(daysTill);
+
+console.log(daysTill);
+
+//365-18=347 (12-13-14)
+//365-55=310 (11-6-14)
+
 
