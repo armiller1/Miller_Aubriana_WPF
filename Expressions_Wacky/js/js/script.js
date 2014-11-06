05fe5bbf43988808136f7f2881bb084889667989
armiller1 <aubriana.miller@yahoo.com>
2014-11-06 17:36:51 -0500

    Expressions_Wacky

---
+++ Expressions_Wacky/js/js/script.js
@@ -0,0 +1,22 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Assignment: Expressions
+ 11-6-2014
+ */
+
+//Calculating how many stars there are in the galaxy (not actual)
+
+var numStars = 200;
+prompt("How many stars are in 2000 miles of the galaxy?");//In this case it's 200
+var miles = 2000;//How many miles that contain 200 stars
+
+var galaxy = 100000;//How many miles are in the galaxy
+prompt("How many miles are in the galaxy?");//In this case it's 100,000
+var answer = galaxy / miles * numStars;//Calculating answer
+
+alert("There are " + answer + " stars in the galaxy.");//Alert of answer
+
+console.log(answer);
+
+
---
+++ Expressions_Wacky2/js/script.js
@@ -0,0 +1,8 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Assignment: Expressions
+ 11-6-2014
+ */
+
+
