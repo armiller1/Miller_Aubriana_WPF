ecc088bad9e4af7ae470c6fa30754c76693add45
armiller1 <aubriana.miller@yahoo.com>
2014-11-06 18:46:03 -0500

    Expressions_Personal
    Correct submission

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,25 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Expressions_Personal
+ 11-6-2014
+ */
+
+//How many Days until your Anniversary
+var anniVers = 347;
+prompt("When is your Anniversary?");
+
+var currDate = 310;//today's date
+prompt("What is today's date?");
+
+var daysTill = anniVers - currDate + " days until your Anniversary!";
+
+alert(daysTill);
+
+console.log(daysTill);
+
+//365-18=347 (12-13-14)
+//365-55=310 (11-6-14)
 
 
 
