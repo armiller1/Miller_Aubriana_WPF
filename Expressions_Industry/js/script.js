b9faf97b87b00d393e54e4a8ae4d3303373cef36
armiller1 <aubriana.miller@yahoo.com>
2014-11-06 18:22:59 -0500

    Expressions Industry
    Correct submission

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,4 +7,15 @@
 
 //Determining the area of a monitor screen
 
+var height = 8.5;//Height of screen is 8.5in
+prompt("What is the height?");//Asking user for height
+
+var width = 13;//Width of screen is 13in
+prompt("What is the width?");//Asking user for width
+
+var area = height * width;//Multiplying height and width for area
+alert("The area of your computer screen is " + area + " sq in");//Alerting user of answer
+
+console.log(area);
+
 
