2bba79d244dc24ede35480d98cd2db6fe2c41036
armiller1 <aubriana.miller@yahoo.com>
2014-11-06 23:51:02 -0500

    Shopping bill

---
+++ Expressions_Worksheet_Average Shopping Bill/js/script.js
@@ -0,0 +1,20 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Assignment: Expressions
+ 11-6-2014
+ */
+
+var amount = prompt("List your last five receipt amounts (with commas in between)");
+ //[54.96, 42.73, 48.60, 51.49, 61.54];
+var total = amount[0] + amount[1] + amount[2] + amount[3] + amount[4];
+
+alert("The total amount of money spent on groceries over the past five weeks is " + total);
+//var mean;
+//mean = total / 5;
+
+console.log(total);
+
+
+
+
