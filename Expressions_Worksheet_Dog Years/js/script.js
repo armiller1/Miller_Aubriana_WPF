a5b6b1c15751a6f46bd7086395dfa1afa8010f16
armiller1 <aubriana.miller@yahoo.com>
2014-11-06 20:04:00 -0500

    Dog Years
    Calculated as if Sparky was 5 in human years

---
+++ Expressions_Worksheet_Dog Years/js/script.js
@@ -0,0 +1,16 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Assignment: Expressions
+ 11-6-2014
+ */
+
+//Conversion from human years to dog years
+
+var sparkyAge = 5;//This is Sparkys age in human years
+prompt("How old is Sparky in human years?");//In this case it's 5
+var dogYears = sparkyAge * 7;//This is how the age will be calculated into dog years
+alert("Sparky is " + dogYears + " in dog years.");//This will alert the user of the answer
+
+console.log(dogYears);
+
