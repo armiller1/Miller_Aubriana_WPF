32fd2e6a33057363e615921d0682a8325a9c2322
armiller1 <aubriana.miller@yahoo.com>
2014-11-09 20:10:38 -0500

    Screencast: If and Else

---
+++ Screencast_If and Else/js/script.js
@@ -0,0 +1,21 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: If and Else
+ 11-9-2014
+ */
+
+//Conditional Logic - With an Expression
+
+var kidHeight = 52;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+ //code performed if condition is true
+ console.log("You can ride the coaster!");
+}else{
+ //code performed if condition is false
+ console.log("Sorry kid, you've got some growing to do first!");
+}
