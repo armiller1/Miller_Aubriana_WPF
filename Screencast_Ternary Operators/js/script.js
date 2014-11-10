d2e02a58423f3c10c19a016cf25acffdedcbcf4c
armiller1 <aubriana.miller@yahoo.com>
2014-11-10 14:37:25 -0500

    Screencast: ternary Operators

---
+++ Screencast_Ternary Operators/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: Ternary Operators
+ 11-10-2014
+ */
+
+//Conditional Logic - Ternary Operators
+
+var age = 6;
+var book;
+
+//If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
