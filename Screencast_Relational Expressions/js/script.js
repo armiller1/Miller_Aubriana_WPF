657a651b70e2258c8b36194055d92d592410f24f
armiller1 <aubriana.miller@yahoo.com>
2014-11-09 19:44:41 -0500

    Screencast: Relational Expressions

---
+++ Screencast_Relational Expressions/js/script.js
@@ -0,0 +1,18 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: Relational Expressions
+ 11-9-2014
+ */
+
+//Conditional Logic - Relational Expressions
+
+var kidHeight = 40;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is tru
+    console.log("You can ride the coaster!");
+}
