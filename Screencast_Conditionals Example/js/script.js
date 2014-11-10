8d65287465cd6077049ea92a268faa6ab2502291
armiller1 <aubriana.miller@yahoo.com>
2014-11-09 19:33:51 -0500

    Screencast Conditional Example

---
+++ Screencast_Conditionals Example/js/script.js
@@ -0,0 +1,14 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: Conditionals Example
+ 11-6-2014
+ */
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
