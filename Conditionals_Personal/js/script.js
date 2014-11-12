719966c59aeae385318d22c001b55f4b5b1ffa04
armiller1 <aubriana.miller@yahoo.com>
2014-11-11 19:30:55 -0500

    What shoes should I wear today?
    erased 2 variables and added alerts to if and else if

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,11 +7,14 @@
 
 //What shoes should I wear today?
 
-var conVerse = alert("It's too cold, wear Converse!");
-var flipFlops = alert("It's nice and warm, you can wear flip flops!");
 var weather = prompt("What is the temperature right now in Fahrenheit?");
+var sunny = prompt("Is it sunny?");
 
-if(weather <= 70){
-    alert(conVerse);
-    console.log(conVerse);
+
+if(weather <= 70 && sunny === "no"){
+    alert("It's too cold, you should wear Converse!");
+    console.log("It's too cold, you should wear Converse!");
+}else if(weather >= 69 && sunny === "yes"){
+    alert("It's nice and warm, you can wear flip flops!");
+    console.log("It's nice and warm, you can wear flip flops!")
 }
