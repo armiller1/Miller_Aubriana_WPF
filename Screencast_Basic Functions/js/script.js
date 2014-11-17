884dcbd3bc20b6e0f4c0174ea92dfcaa500d4b89
armiller1 <aubriana.miller@yahoo.com>
2014-11-17 16:00:47 -0500

    Screencast: Basic Functions

---
+++ Screencast_Basic Functions/js/script.js
@@ -0,0 +1,19 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: Basic Functions
+ 11-17-2014
+ */
+
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World!");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
