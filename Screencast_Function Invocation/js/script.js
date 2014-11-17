829614b765af09b2eeb8ec3a85975879743494e4
armiller1 <aubriana.miller@yahoo.com>
2014-11-17 16:16:31 -0500

    Screencast: Function invocation

---
+++ Screencast_Function Invocation/js/script.js
@@ -0,0 +1,23 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+Screencast: Function Invocation
+ 11-17-2014
+ */
+
+//Functions - Function Execution
+
+function outptMsg(){
+ console.log("Hello World!");
+}
+
+function calcArea(){
+ var width = 20;
+ var height = 30;
+ var area = width * height;
+ console.log(area);
+}
+
+calcArea();
+calcArea();
+calcArea();
