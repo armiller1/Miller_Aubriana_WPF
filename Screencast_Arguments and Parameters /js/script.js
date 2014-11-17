d0d173841e08e1b91e5383a8be29e426c35dc733
armiller1 <aubriana.miller@yahoo.com>
2014-11-17 16:50:12 -0500

    Screencast: Arguments and Parameters

---
+++ Screencast_Arguments and Parameters /js/script.js
@@ -0,0 +1,16 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: Arguments and Parameters
+ 11-17-2014
+ */
+
+//Functions - Parameters and Arguments
+
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.");
+}
+var age1 = 4;
+dogYears(age1); //arguments
+dogYears(5);
