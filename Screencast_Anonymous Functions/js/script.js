02d6a98ba0a5e3bf2a87c09bd44a29107b73d608
armiller1 <aubriana.miller@yahoo.com>
2014-11-17 17:17:36 -0500

    Screencast: Anonymous Functions

---
+++ Screencast_Anonymous Functions/js/script.js
@@ -0,0 +1,18 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: Anonymous Functions
+ 11-17-2014
+ */
+
+//Functions - Anonymous Functions
+
+var calcArea = function(width, height){ //defining
+//code the function runs
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
