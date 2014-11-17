cbaf64b10c0ab70d7d25e5aa68021e55dcb783b8
armiller1 <aubriana.miller@yahoo.com>
2014-11-17 16:58:23 -0500

    Screencast: Returning Values

---
+++ Screencast_Returning Values/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: Returning Values
+ 11-17-2014
+ */
+
+//Functions - Returning Values
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+    return area; //function spitting the info out
+}
+console.log(total);
