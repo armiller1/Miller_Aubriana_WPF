1bcf39db96d2ffcc314a925fc1689189324119cb
armiller1 <aubriana.miller@yahoo.com>
2014-11-18 17:26:24 -0500

    Screencast: Do While Loops

---
+++ Screencast_Do While Loops/js/script.js
@@ -0,0 +1,25 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Screencast: While Loops
+ 11-18-2014
+ */
+
+//While Loops
+
+var b = 50; //sets up the index
+
+while(b < 0){ //checks the condition
+    console.log(b + " kegs on the wall");
+    b--; //increments or decrements the index
+}
+
+//Do-While Loops
+
+var c = 10;
+
+do{
+    console.log(c + " kegs on the wall");
+    c--;
+}
+while(c > 0);
