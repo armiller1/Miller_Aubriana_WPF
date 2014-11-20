5656d632c954203a14888889e73262d1c7cc76ca
armiller1 <aubriana.miller@yahoo.com>
2014-11-20 08:45:10 -0500

    Chickenpox

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,21 +7,21 @@
 
 //How many chicken pox did you have?
 
-var age = prompt("How old were you when you got chicken pox?");
+var age = prompt("How old were you when you got chicken pox?");//asks user for age when they had chicken pox
 
-var pox1 = 10;
-var pox2 = 100;
+var pox1 = 10;//lowest number of chicken pox
+var pox2 = 100;//highest number of chicken pox
 
-while(isNaN(age) || age===""){
-    prompt("You must enter an age!")
+while(isNaN(age) || age===""){//while inputted answer is blank or not a nymber
+    prompt("You must enter an age!")//reminds user to enter age
 }
-randomize(10,100);
+randomize(10,100);//calls function
 
-var result = randomize(pox1,pox2);
-alert("You had " + result + " chicken pox.");
+var result = randomize(pox1,pox2);//result variable
+alert("You had " + result + " chicken pox.");//alerts user of how many chicken pox they had
 console.log("You had " + result + " chicken pox.");
 
-function randomize(pox1,pox2){
+function randomize(pox1,pox2){//Function
     var randomNum = Math.round(Math.random()*(pox2-pox1)+Number(pox1));
-    return randomNum;
+    return randomNum;//Return results
 }
