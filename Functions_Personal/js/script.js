af75f9dbb536933b4fe81ea8410174f64ae5d5eb
armiller1 <aubriana.miller@yahoo.com>
2014-11-20 06:44:56 -0500

    How many kids

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,21 +7,22 @@
 
 //How many children will I have?
 
-var age = prompt("How old are you?");
-var kids1 = 1;
-var kids2 = 15;
+var age = prompt("How old are you?");//asks user for age
+var kids1 = 1;//lowest number of children
+var kids2 = 15;//highest number of children
 
-while(isNaN(age) || age===""){
-    prompt("You must enter an age!");
+while(isNaN(age) || age===""){//while inputted answer is blank or not a number
+    prompt("You must enter an age!");//reminds user to enter age
 }
 
-randomize(1,14);
+randomize(1,14);//calls function
 
-var result = randomize(kids1,kids2);
-alert("You will have " + result + " children.");
+var result = randomize(kids1,kids2);//variable for result
+alert("You will have " + result + " children.");//alerts user of number of children
 console.log("You will have " + result + " children.");
 
+//function generating result
 function randomize(kids1,kids2){
     var randomNumber = Math.round(Math.random()*(kids2-kids1)+Number(kids1));
-    return randomNumber;
+    return randomNumber;//returns results
 }
