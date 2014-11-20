f1dd78b53cb28fe831d0b51f114b15fadefdc8b9
armiller1 <aubriana.miller@yahoo.com>
2014-11-20 08:01:37 -0500

    Building website

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -7,25 +7,26 @@
 
 //How long will it take to build the website?
 
-var siteType = prompt("Is the website personal or for a business?");
-var numPages = prompt("How many pages are needed on this website?");
+var siteType = prompt("Is the website personal or for a business?");//type of site
+var numPages = prompt("How many pages are needed on this website?");//how big is the site
+//lowest to most amount of days to build site
 var days1 = 14;
 var days2 = 90;
-
-while(siteType ==="" || siteType != "personal" && siteType != "business"){
+//while user prompt is different than available answers or left unanswered
+while(siteType ==="" || siteType != "personal" || siteType != "business"){
     prompt("Is the website personal or for a business?");
     console.log("Is the website personal or for a business?");
 }while(isNaN(numPages) || numPages ===""){
     prompt("How many pages are needed on this website?");
     console.log("How many pages are needed on this website?");
 }
-determine(14,90);
+determine(14,90);//calls the function
 
-var result = determine(days1,days2);
-alert("Your project will take approximately " + result +" days to complete!");
+var result = determine(days1,days2);//variable for result
+alert("Your project will take approximately " + result +" days to complete!");//alerting user of results
 console.log("Your project will take approximately " + result +" days to complete!");
 
-function determine(days1,days2){
+function determine(days1,days2){//function
     var numDays = Math.round*(days2-days1)+Number(days1);
     return numDays;
 }
