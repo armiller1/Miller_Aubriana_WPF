a1a20bf5056dd7fb124a655b6189077f6245c5fd
armiller1 <aubriana.miller@yahoo.com>
2014-11-20 06:39:08 -0500

    How many kids

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -15,6 +15,13 @@ while(isNaN(age) || age===""){
     prompt("You must enter an age!");
 }
 
+randomize(1,14);
+
 var result = randomize(kids1,kids2);
 alert("You will have " + result + " children.");
 console.log("You will have " + result + " children.");
+
+function randomize(kids1,kids2){
+    var randomNumber = Math.round(Math.random()*(kids2-kids1)+Number(kids1));
+    return randomNumber;
+}
