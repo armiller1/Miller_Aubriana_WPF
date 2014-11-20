b1300fd74a093fae568efe0d38e9ea2c5cd6284f
armiller1 <aubriana.miller@yahoo.com>
2014-11-20 07:49:57 -0500

    Building website

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Aubriana Miller
+ WPF 1411 Section 01
+ Functions: Industry
+ 11-18-2014
+ */
+
+//How long will it take to build the website?
+
+var siteType = prompt("Is the website personal or for a business?");
+
+
+
+
+
